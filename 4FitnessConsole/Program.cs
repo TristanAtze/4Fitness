0ec582492c16164a2df34be2704bf3e406c4d635
TristanAtze <ben.sowieja@4sellers.de>
2025-03-06 09:21:58 +0100

    v

--- 4FitnessConsole/Program.cs
+++ 4FitnessConsole/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(          66.47 + (13.7 * 88) + (5 * 189) - (6.8 * 17)                     );
+Console.ReadKey();
