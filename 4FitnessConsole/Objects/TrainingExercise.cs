79f4f7c729e6933c6f0e90d3b63b6af5e3e8f223
Lorenz Kupfer <lorenz.kupfer@4sellers.de>
2025-03-06 13:12:11 +0100

    Implemented Objects

---
+++ 4FitnessConsole/Objects/TrainingExercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4FitnessConsole.Objects
+{
+    class TrainingExercise(int id,
+        string name,
+        int repetitions,
+        int sets,
+        string difficulty,
+        string explanation)
+    {
+        public int ID { get; set; } = id;
+
+        public string Name { get; set; } = name;
+
+        public int Repetitions { get; set; } = repetitions;
+
+        public int Sets { get; set; } = sets;
+
+        public string Difficulty { get; set; } = difficulty;
+
+        public string Explanation { get; set; } = explanation;
+    }
+}
---
+++ 4FitnessConsole/Objects/TrainingPlan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4FitnessConsole.Objects
+{
+    class TrainingPlan(int id,
+        int personId,
+        TrainingExercise[] content)
+    {
+        public int ID { get; set; } = id;
+
+        public int PersonID { get; set; } = personId;
+
+        public TrainingExercise[] Content { get; set; } = content;
+    }
+}
