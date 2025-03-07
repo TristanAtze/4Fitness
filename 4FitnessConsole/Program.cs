5c945e292c70557b7e65ef9b96f53600b762588e
TristanAtze <ben.sowieja@4sellers.de>
2025-03-07 07:57:56 +0100

    Calculator finish + Person Construktor

--- 4FitnessConsole/Program.cs
+++ 4FitnessConsole/Program.cs
@@ -1,6 +1,14 @@
 ﻿using BasicProgram;
 using BasicProgram.Objects;
 
-var person = new Person(88,189,17,Person.GenderEnum.Male, Person.ExpEnum.Intermediate, Person.ActEnum.moderate, (Person.GoalEnum.MuskelnAufbauen, null));
+var person = new Person(88,189,17,Person.GenderEnum.Male, Person.ExpEnum.Intermediate, Person.ActEnum.moderate, (Person.GoalEnum.MuskelnAufbauen, 0.3));
 
-double kalorien = Calculator.CalculateCaloriNeed(person); 
+(double, string) bmi = Calculator.CalculateBMI(person);
+Console.WriteLine($"BMI: {bmi.Item1} - {bmi.Item2}");
+double protein = Calculator.CalculateProtein(person);
+Console.WriteLine($"Proteinbedarf: {protein} g");
+double kreatin = Calculator.CalculateKreatin(person);
+Console.WriteLine($"Kreatinbedarf: {kreatin} g");
+double kalorien = Calculator.CalculateCaloriNeed(person);
+Console.WriteLine($"Kalorienbedarf: {kalorien} kcal");
+Console.ReadKey();
